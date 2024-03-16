ea9c5d4ae1d92e7decefdfe3edab29f231a8f8c6
niczyji <niczyji@gmail.com>
2024-03-16 23:04:32 +0100

    feat: enhance navigation with mobile dropdown and dynamic booking link

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -10,13 +10,37 @@
         <li><router-link to="/services">Behandlungen</router-link></li>
         <li><router-link to="/prices">Preise</router-link></li>
         <li><router-link to="/gallery">Galerie</router-link></li>
-        <li><a href="https://www.planity.com/de-DE/kosmetik-studio-excellent-50670-koln" target="_blank" rel="noopener noreferrer">Termin Buchen</a></li>      </ul>
+        <li>
+          <a :href="bookingUrl" target="_blank" rel="noopener noreferrer">Termin Buchen</a>
+        </li>
+      </ul>
     </nav>
+    <div class="icon">
+      <i @click="toggleMobileNav" v-show="mobile" class="far fa-bars" :class="{ 'icon-active': mobileNav }"></i>
+    </div>
+    <transition name="mobile-nav">
+      <ul v-show="mobile" class="dropdown-nav">
+        <li><router-link to="/">Startseite</router-link></li>
+        <li><router-link to="/about-us">Über uns</router-link></li>
+        <li><router-link to="/services">Behandlungen</router-link></li>
+        <li><router-link to="/prices">Preise</router-link></li>
+        <li><router-link to="/gallery">Galerie</router-link></li>
+        <li>
+          <a :href="bookingUrl" target="_blank" rel="noopener noreferrer">Termin Buchen</a>
+        </li>
+      </ul>
+    </transition>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      bookingUrl: 'https://www.planity.com/de-DE/kosmetik-studio-excellent-50670-koln'
+    };
+  }
+};
 </script>
 
 <style lang="sass" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,11 @@
   <div class="home">
     <section class="hero">
       <div class="hero-text container">
-        <h4>Vue Responsive Nav Menu</h4>
+        <h4>Entdecken Sie Ihr Strahlen im</h4>
         <hr />
         <h2>
-          With Vue <br />
-          Animations
+          Kosmetik Studio <br />
+          EXCELLENT
         </h2>
         <hr />
       </div>
