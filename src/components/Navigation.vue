0f40444a3151f28edfa3c25c9da018c895a5de3d
niczyji <niczyji@gmail.com>
2024-04-04 21:52:51 +0200

    refactor: enhanced mobile navigation experience with TailwindCSS transitions

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -16,25 +16,43 @@
 
       <ul v-show="!mobile" class="navigation lg:space-y-0 lg:flex lg:items-center lg:justify-between">
         <li class="inline-block px-1 py-2 sm:px-2 lg:px-3 sm:ml-2 lg:ml-4">
-          <router-link class="text-black link hover:text-coffee-brown" :to="{ name: 'Home' }">Startseite</router-link>
+          <router-link
+            class="text-black link hover:text-coffee-brown hover:underline underline-offset-2"
+            :to="{ name: 'Home' }"
+            >Startseite</router-link
+          >
         </li>
         <li class="inline-block px-1 py-2 sm:px-2 lg:px-3 sm:ml-2 lg:ml-4">
-          <router-link class="text-black link hover:text-coffee-brown" :to="{ name: 'AboutUs' }">Über uns</router-link>
+          <router-link
+            class="text-black link hover:text-coffee-brown hover:underline underline-offset-2"
+            :to="{ name: 'AboutUs' }"
+            >Über uns</router-link
+          >
         </li>
         <li class="inline-block px-1 py-2 sm:px-2 lg:px-3 sm:ml-2 lg:ml-4">
-          <router-link class="text-black link hover:text-coffee-brown" :to="{ name: 'Services' }"
+          <router-link
+            class="text-black link hover:text-coffee-brown hover:underline underline-offset-2"
+            :to="{ name: 'Services' }"
             >Behandlungen</router-link
           >
         </li>
         <li class="inline-block px-1 py-2 sm:px-2 lg:px-3 sm:ml-2 lg:ml-4">
-          <router-link class="text-black link hover:text-coffee-brown" :to="{ name: 'Prices' }">Preise</router-link>
+          <router-link
+            class="text-black link hover:text-coffee-brown hover:underline underline-offset-2"
+            :to="{ name: 'Prices' }"
+            >Preise</router-link
+          >
         </li>
         <li class="inline-block px-1 py-2 sm:px-2 lg:px-3 sm:ml-2 lg:ml-4">
-          <router-link class="text-black link hover:text-coffee-brown" :to="{ name: 'Gallery' }">Galerie</router-link>
+          <router-link
+            class="text-black link hover:text-coffee-brown hover:underline underline-offset-2"
+            :to="{ name: 'Gallery' }"
+            >Galerie</router-link
+          >
         </li>
         <li class="inline-block px-1 py-2 sm:px-2 lg:px-3 sm:ml-2 lg:ml-4">
           <a
-            class="px-4 py-2 text-black no-underline transition-colors rounded-md link book-appointment hover:bg-coffee-brown hover:text-white"
+            class="px-4 py-2 text-black transition-shadow bg-white rounded-md shadow-sm hover:underline underline-offset-2 hover:text-coffee-brown hover:bg-coffee-brown hover:shadow-lg"
             :href="bookingUrl"
             target="_blank"
             rel="noopener noreferrer"
@@ -55,44 +73,95 @@
         </button>
       </div>
 
-      <transition name="mobile-nav">
+      <transition
+        name="slide-in"
+        enter-active-class="transition duration-300 ease-out"
+        enter-from-class="transform -translate-x-full"
+        enter-to-class="transform translate-x-0"
+        leave-active-class="transition duration-300 ease-in"
+        leave-from-class="transform translate-x-0"
+        leave-to-class="transform -translate-x-full"
+      >
         <ul
           v-show="mobileNav"
-          class="fixed absolute top-0 left-0 z-50 flex flex-col w-full h-full max-w-xs mt-1 bg-white shadow-md dropdown-nav top-full"
+          class="fixed top-0 left-0 z-50 flex flex-col w-full h-full bg-white border-r border-black shadow-md max-w-48"
         >
-          <li class="border-b border-gray-200 py-2 px-2.5 ml-3.5">
-            <router-link class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Home' }"
+          <li class="border-b border-gray-200 py-2 px-2.5 mx-3.5">
+            <router-link class="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Home' }"
               >Startseite</router-link
             >
           </li>
-          <li class="border-b border-gray-200 py-2 px-2.5 ml-3.5">
-            <router-link class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'AboutUs' }"
+          <li class="border-b border-gray-200 py-2 px-2.5 mx-3.5">
+            <router-link class="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'AboutUs' }"
               >Über uns</router-link
             >
           </li>
-          <li class="border-b border-gray-200 py-2 px-2.5 ml-3.5">
-            <router-link class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Services' }"
+          <li class="border-b border-gray-200 py-2 px-2.5 mx-3.5">
+            <router-link class="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Services' }"
               >Behandlungen</router-link
             >
           </li>
-          <li class="border-b border-gray-200 py-2 px-2.5 ml-3.5">
-            <router-link class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Prices' }"
+          <li class="border-b border-gray-200 py-2 px-2.5 mx-3.5">
+            <router-link class="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Prices' }"
               >Preise</router-link
             >
           </li>
-          <li class="border-b border-gray-200 py-2 px-2.5 ml-3.5">
-            <router-link class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Gallery' }"
+          <li class="border-b border-gray-200 py-2 px-2.5 mx-3.5">
+            <router-link class="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100" :to="{ name: 'Gallery' }"
               >Galerie</router-link
             >
           </li>
-          <li class="py-2 px-2.5 ml-3.5">
+          <li class="py-2 px-2.5 mx-3.5">
             <a
-              class="block px-4 py-2 text-sm text-black text-gray-700 no-underline hover:bg-gray-100"
+              class="block px-2 py-2 text-sm text-black text-gray-700 underline underline-offset-2 hover:bg-gray-100"
               :href="bookingUrl"
               target="_blank"
               rel="noopener noreferrer"
               >Termin Buchen</a
             >
+          </li>
+          <li class="py-2 border-t border-gray-200">
+            <div class="flex items-center justify-center space-x-4">
+              <!-- Telefon -->
+              <a
+                href="tel:+491787046251"
+                class="flex items-center transition-transform hover:text-black hover:scale-110"
+              >
+                <img src="@/assets/call-outline.svg" alt="Telefon" class="w-6 h-6 mr-2" />
+              </a>
+              <!-- E-Mail -->
+              <a
+                href="mailto:excellent-center@web.de"
+                class="flex items-center transition-transform hover:text-black hover:scale-110"
+              >
+                <img src="@/assets/mail-outline.svg" alt="E-Mail" class="w-6 h-6 mr-2" />
+              </a>
+              <!-- Instagram -->
+              <a
+                href="https://www.instagram.com/kosmetikstudiokoeln"
+                class="transition-transform hover:text-black hover:scale-110"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-6 h-6 mr-2"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M7.5 3C4.462 3 2 5.462 2 8.5v7C2 18.538 4.462 21 7.5 21h9c3.038 0 5.5-2.462 5.5-5.5v-7C22 5.462 19.538 3 16.5 3h-9z"
+                  />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M16 11.37a4 4 0 11-8 0 4 4 0 018 0zM18.5 7.5h.01"
+                  />
+                </svg>
+              </a>
+            </div>
           </li>
         </ul>
       </transition>
@@ -147,21 +216,6 @@ export default {
 
 <style lang="scss" scoped>
 header {
-  /* .mobile-nav-enter-active,
-    .mobile-nav-leave-active {
-      transition: 1s ease all;
-    }
-
-    .mobile-nav-enter-form,
-    .mobile-nav-leave-to {
-      transform: translateX(-250px);
-    }
-
-    .mobile-nav-enter-to {
-      transform: translateX(0);
-    }
-  } */
-
   .hamburger {
     cursor: pointer;
     width: 25px;
